ebeb6dbf4672b94083e591ac13b8316f7894baf6
kingsoftech <asoyero@student.lautech.edu.ng>
2024-03-05 02:52:20 +0100

    donekit update

--- dronekit_python/first_dk_script.py
+++ dronekit_python/first_dk_script.py
@@ -1,5 +1,6 @@
 from dronekit import connect
 
-##Connecting the drone to script(Using UDP end point)
-
+# Connect to UDP endpoint.
 vehicle = connect('127.0.0.1:14550', wait_ready=True)
+# Use returned Vehicle object to query device state - e.g. to get the mode:
+print("Mode: %s" % vehicle.mode.name)
